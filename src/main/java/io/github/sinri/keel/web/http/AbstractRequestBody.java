99e345610559dbfc655096ba669efa5d6b0a1a8b
sinri <e.joshua.s.e@gmail.com>
2024-06-18 11:44:45 +0800

    3.2.13-SNAPSHOT io.github.sinri.keel.web.http.AbstractRequestBody constructor check content type header
    Signed-off-by: sinri <e.joshua.s.e@gmail.com>

--- src/main/java/io/github/sinri/keel/web/http/AbstractRequestBody.java
+++ src/main/java/io/github/sinri/keel/web/http/AbstractRequestBody.java
@@ -1,13 +1,32 @@
 package io.github.sinri.keel.web.http;
 
 import io.github.sinri.keel.core.json.SimpleJsonifiableEntity;
+import io.vertx.core.json.JsonObject;
 import io.vertx.ext.web.RoutingContext;
 
 /**
- * @since 3.0.1
+ * @since 3.0.1 JSON only.
+ * @since 3.2.13 automatically support JSON and FORM.
  */
 abstract public class AbstractRequestBody extends SimpleJsonifiableEntity {
     public AbstractRequestBody(RoutingContext routingContext) {
-        super(routingContext.body().asJsonObject());
+        //super(routingContext.body().asJsonObject());
+        super();
+        if (routingContext != null) {
+            String contentType = routingContext.request().headers().get("Content-Type");
+            if (contentType != null) {
+                if (contentType.contains("multipart/form-data")) {
+                    routingContext.request().setExpectMultipart(true);
+                }
+                if (contentType.contains("application/json")) {
+                    this.reloadDataFromJsonObject(routingContext.body().asJsonObject());
+                } else {
+                    JsonObject requestObject = new JsonObject();
+                    routingContext.request().formAttributes()
+                            .forEach(entry -> requestObject.put(entry.getKey(), entry.getValue()));
+                    this.reloadDataFromJsonObject(requestObject);
+                }
+            }
+        }
     }
 }
